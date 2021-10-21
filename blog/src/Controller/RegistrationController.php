ea88ff7089b9a905deb834aeb66fb43ec9c401bb
MinoriMind <similianka@gmail.com>
2021-10-21 15:24:54 +1000

    registration added

---
+++ blog/src/Controller/RegistrationController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegistrationType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RegistrationController extends AbstractController
+{
+    #[Route('/register', name: 'register')]
+    public function register(Request $request): Response
+    {
+        $user = new User();
+        $form = $this->createForm(RegistrationType::class, $user);
+        $form->handleRequest($request);
+        if($form->isSubmitted())
+        {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+        }
+
+        return $this->render('registration/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
